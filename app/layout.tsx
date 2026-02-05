c4f64a6aec0e1b60558fb6f01a4cb227e3e64803
ahmed hassan <you@example.com>
2026-02-06 04:13:16 +0500

    fianl

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
-import GoogleAnalytics from './Components/GoogleAnalytics'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
